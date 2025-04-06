23679b7bbd8e22628f26444dff87f78271f17c6d
ghadgerahulaws <ghadgerahulaws@gmail.com>
2025-04-06 14:14:16 +0530

    video 14 by nana

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "instance_type" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -59,7 +60,7 @@ resource "aws_route_table_association" "a-rtb-association" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
   route = {
     cidr_block = "0.0.0.0/16"
-    gateway_id = aws_internet_gateway.id
+    gateway_id = aws_internet_gateway.id6
   }
 
   tags = {
@@ -97,4 +98,57 @@ resource "aws_security_group" "myapp-sg" {
       Name: "${var.env_prefix}-sg"
     }
   
+}
+
+data "aws_ami" "latest-amazon-ami" {
+     most_recent = true
+     owners = ["amazon"]
+     filter {
+       name = "name"
+       values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+      
+     }
+
+     filter {
+       name = "virtualization-type"
+       values = ["hvm"]
+     }
+
+}
+
+output "aws_ami" {
+  value = data.aws_ami.latest-amazon-ami
+  
+}
+resource "aws_key_pair" "myapp-key" {
+  key_name = "myapp-key"
+  public_key = file("terra.pub")
+  
+}
+resource "aws_instance" "myapp-ec2" {
+
+    ami = data.aws_ami.latest-amazon-ami.id 
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = aws_key_pair.myapp-key.key_name
+
+
+      #for small data we can write this way
+    /*user_data = <<EOF
+                #!/bin/bash
+                sudo yum update -y && yum install -y docker
+                sudo systemctl start docker
+                sudo useradd -aG docker ec2-user
+                docker run -p 8080:80 nginx
+
+                EOF
+                */
+      user_data = file("bash_userdata_script.sh")
+
+    tags = {
+      Name: "${var.env_prefix}-ec2"
+    }
 }
